64c605e2100e58ab3812c21f4f3a551a4b64bc8d
Mykhaylo M. Malakhov <malak039@umn.edu>
2022-11-20 14:46:41 -0500

    New test statistic

--- src/drab.R
+++ src/drab.R
@@ -11,16 +11,16 @@ ReadPlink <- function(root)
   bimfile <- paste(root, ".bim", sep = "")
   
   # Import bed/bim/fam files
-  # Note that specifying n and p speeds up BEDMatrix, since otherwise it would read the fam and bim files again.
   bim <- read.table(bimfile, header = FALSE, stringsAsFactors = FALSE)
   fam <- read.table(famfile, header = FALSE, stringsAsFactors = FALSE)
+  # Note that specifying n and p speeds up BEDMatrix, since otherwise it would read the fam and bim files again
   bed <- as.data.frame(as.matrix(BEDMatrix::BEDMatrix(bedfile, n = nrow(fam), p = nrow(bim))))
   
-  # Add individual and variant names to the imported bed.
-  # Individual names are coded as FID_IID;
-  # Variant names are coded as rsid_A1.
+  # Add individual and variant names to the imported bed:
+  # Individual names are coded as FID_IID
+  # Variant names are coded the same as in the original plink file
   rownames(bed) <- paste(fam$V1, fam$V2, sep = "_")
-  colnames(bed) <- paste(bim$V2, bim$V5, sep = "_")
+  colnames(bed) <- bim$V2
   
   # Set row/column names and remove unused columns in the imported fam
   rownames(fam) <- paste(fam$V1, fam$V2, sep = "_")
@@ -80,7 +80,7 @@ CovarAdjust <- function(plink_path, covar_path)
   return(list(genotypes = genotypes, expression = expression))
 }
 
-# Train and test an expression imputation model, returning the squared prediction residuals
+# Train and test a transcriptome imputation model, returning the squared prediction residuals
 TrainTest <- function(training_genotypes, training_expression, testing_genotypes, testing_expression)
 {
   # Fit an elastic net regression model on the training data
@@ -122,33 +122,41 @@ data_train_1$genotypes <- subset(data_train_1$genotypes, select = all_snps)
 data_train_2$genotypes <- subset(data_train_2$genotypes, select = all_snps)
 data_test$genotypes <- subset(data_test$genotypes, select = all_snps)
 
-# Find the sample mean and variance of the differences between model prediction errors on the full dataset
+# Save sample sizes
+n_training <- nrow(data_train_1$expression)
+n_testing <- nrow(data_test$expression)
+
+# Train models on the full data set, then find the average difference between model prediction errors
 loss_full_1 <- TrainTest(data_train_1$genotypes, data_train_1$expression$value, data_test$genotypes, data_test$expression$value)
 loss_full_2 <- TrainTest(data_train_2$genotypes, data_train_2$expression$value, data_test$genotypes, data_test$expression$value)
-var_differences_full <- var(loss_full_1 - loss_full_2)
 mean_differences_full <- mean(loss_full_1 - loss_full_2)
 
-# Find the distribution of sample means of the differences between model prediction errors using bootstrapping
+# Use bootstrapping to approximate the sampling distributions (across all possible training sets) of:
+# (1) the sample mean of the differences between model prediction errors
+# (2) the sample variance of the mean difference between model prediction errors
 boot_means <- numeric(boot)
+boot_variances <- numeric(boot)
 for (i in 1:boot) {
-  # Sample from the rows of each dataset with replacement
-  resamples_train_1 <- sample(seq_len(nrow(data_train_1$expression)), replace = TRUE)
-  resamples_train_2 <- sample(seq_len(nrow(data_train_2$expression)), replace = TRUE)
-  resamples_test <- sample(seq_len(nrow(data_test$expression)), replace = TRUE)
+  # Sample from the rows of each training data set with replacement
+  resamples_train_1 <- sample(seq_len(n_training), replace = TRUE)
+  resamples_train_2 <- sample(seq_len(n_training), replace = TRUE)
   
-  # Find the sample mean of the differences between model prediction errors on the resampled dataset
-  loss_boot_1 <- TrainTest(data_train_1$genotypes[resamples_train_1, ], data_train_1$expression[resamples_train_1, ], data_test$genotypes[resamples_test, ], data_test$expression[resamples_test, ])
-  loss_boot_2 <- TrainTest(data_train_2$genotypes[resamples_train_2, ], data_train_2$expression[resamples_train_2, ], data_test$genotypes[resamples_test, ], data_test$expression[resamples_test, ])
+  # Train models on the resampled data sets, then compute their losses on the full testing set
+  loss_boot_1 <- TrainTest(data_train_1$genotypes[resamples_train_1, ], data_train_1$expression[resamples_train_1, ], data_test$genotypes, data_test$expression$value)
+  loss_boot_2 <- TrainTest(data_train_2$genotypes[resamples_train_2, ], data_train_2$expression[resamples_train_2, ], data_test$genotypes, data_test$expression$value)
+  
+  # Calculate the statistics that we are bootstrapping
   boot_means[i] <- mean(loss_boot_1 - loss_boot_2)
+  boot_variances[i] <- var(loss_boot_1 - loss_boot_2) / n_testing
 }
 
 # Conduct the DRAB test, comparing whether the models have equal predictive performance
-n <- nrow(data_test$expression)
-t <- mean_differences_full / sqrt((var_differences_full / n) + var(boot_means))
-pval <- 2 * pt(abs(t), df = n - 1, lower.tail = FALSE)
+training_var <- (var(boot_means) * (n_testing - 1)) / n_testing
+T_DRAB <- (2 * mean_differences_full - mean(boot_means)) / sqrt(mean(boot_variances) + training_var)
+pval <- 2 * pt(abs(t), df = n_testing - 1, lower.tail = FALSE)
 pval_conditional <- t.test(x = loss_full_1, y = loss_full_2, paired = TRUE)$p.value
 
 # Append the test results to the output file
-results <- paste(name, id, pval, pval_conditional, nrow(data_train_1$expression), nrow(data_test$expression), sep = "\t")
+results <- paste(name, id, pval, pval_conditional, n_training, n_testing, sep = "\t")
 cat(results, file = paste("output/", context_A, "-", context_B, "-", out_name, ".txt", sep = ""), append = TRUE, sep = "\n")
 
